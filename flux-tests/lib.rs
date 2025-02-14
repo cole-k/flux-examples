d7b4f17edc9c6aafd5a1bd7cadf2cd83798b0003
Cole K <cole-k@users.noreply.github.com>
2025-02-13 17:25:01 -0800

    init kmeans without any refinements

---
+++ flux-tests/lib.rs
@@ -0,0 +1,14 @@
+#![feature(slice_range)]
+#![feature(extend_one)]
+#![feature(try_reserve_kind)]
+#![feature(allocator_api)]
+#![feature(dropck_eyepatch)]
+#![feature(rustc_attrs)]
+#![feature(core_intrinsics)]
+#![feature(ptr_internals)]
+#![feature(rustc_allow_const_fn_unstable)]
+#![allow(macro_expanded_macro_exports_accessed_by_absolute_paths)]
+#![allow(unused_comparisons)]
+use flux_rs::*;
+pub mod kmeans;
+pub mod rvec;
